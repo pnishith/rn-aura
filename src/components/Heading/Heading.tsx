7d86e834a11a5598668c7af5f83de878a839d24e
Nishith <pnishith@gmail.com>
2026-02-01 19:55:40 +0530

    feat: add Heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Text as RNText, StyleSheet, TextProps as RNTextProps, TextStyle } from 'react-native';
+
+export interface HeadingProps extends RNTextProps {
+  level?: 1 | 2 | 3 | 4 | 5 | 6; // h1 to h6
+  size?: number; // Manual override
+  weight?: TextStyle['fontWeight'];
+  color?: string;
+  align?: TextStyle['textAlign'];
+  style?: TextStyle;
+  children: React.ReactNode;
+}
+
+const LEVEL_SIZES = {
+  1: 32,
+  2: 28,
+  3: 24,
+  4: 20,
+  5: 18,
+  6: 16,
+};
+
+export const Heading: React.FC<HeadingProps> = ({
+  level = 1,
+  size,
+  weight = '700',
+  color = '#111827', // Default dark
+  align = 'left',
+  style,
+  children,
+  ...props
+}) => {
+  const fontSize = size || LEVEL_SIZES[level];
+  const lineHeight = fontSize * 1.25; // Good standard leading
+
+  return (
+    <RNText
+      style={[
+        {
+          fontSize,
+          fontWeight: weight,
+          color,
+          textAlign: align,
+          lineHeight,
+        },
+        style,
+      ]}
+      {...props}
+    >
+      {children}
+    </RNText>
+  );
+};
