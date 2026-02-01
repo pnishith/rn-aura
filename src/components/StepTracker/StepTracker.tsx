96b31ecaf8103e836e54421139171ba7b76f7833
Nishith <pnishith@gmail.com>
2026-02-02 01:02:24 +0530

    chore(StepTracker): update implementation

--- src/components/StepTracker/StepTracker.tsx
+++ src/components/StepTracker/StepTracker.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text, StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, Text, View, type ViewStyle } from 'react-native';
 import Animated, { 
   useSharedValue, 
   useAnimatedStyle, 
